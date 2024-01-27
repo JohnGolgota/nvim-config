a1462c9e986b66197b1898b44662c4c8249b2b31
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-01-27 08:51:31 -0500

    en algun momento lo corrigo... ahora no hay como

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,9 +1,11 @@
-
+-- GLOBAL
 vim.g.mapleader = " "
 
+-- NO SE
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- OTRO QUE NO SE
 vim.o.clipboard = "unnamedplus"
 vim.o.list = true
 vim.o.undofile = true
@@ -13,4 +15,5 @@ vim.o.updatetime = 300
 vim.o.wrap = false
 vim.o.completeopt = "menuone,noselect,noinsert"
 
+-- OTRAS DISTINTAS A LAS ANTERIORES PERO QUE TAMPOCO SE
 vim.cmd([[ let g:instant_username = "JohnGolgota" ]])
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,15 +1,20 @@
 local M = {}
 
-M.telescope = {
-    plugin = true,
+M.general = { n = { ["<C-s>"] = { "<cmd> w <CR>", "Save File" } } }
 
-    n = {
-        ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find Files" },
-        ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find Files" },
-        ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live Grep" },
-        ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Buffers" },
-        ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help Tags" },
+M.telescope = {
+  plugin = true,
+
+  n = {
+    ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find Files" },
+    ["<leader>fa"] = {
+      "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
+      "Find Files",
     },
+    ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live Grep" },
+    ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Buffers" },
+    ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help Tags" },
+  },
 }
 
 return M
