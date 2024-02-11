bba19c21d25f8207c053bc9e3e1362498bdce52c
JohnGolgota <js684new@gmail.com>
2024-02-11 06:08:46 -0500

    no se si ya con esto.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,19 @@
 return {
   "simrat39/rust-tools.nvim",
   "evanleck/vim-svelte",
-  "github/copilot.vim",
+  {
+    "github/copilot.vim",
+    config = function()
+      vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.keymap.set('i', '<tab>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+
+      vim.g.copilot_no_tab_map = false
+    end,
+  },
 }
