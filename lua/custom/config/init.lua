18cbd2f71d74b4303aa146c7e54469f83b74b0c6
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-05-06 17:11:13 -0500

    chore: more save cases

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -1,6 +1,9 @@
 -- General configuration for Neovim
 -- Keymaps
 vim.keymap.set('n', '<C-s>', "<cmd>w<cr><esc>", { desc = 'Save File' })
+vim.keymap.set('i', '<C-s>', "<cmd>w<cr><esc>", { desc = 'Save File' })
+vim.keymap.set('x', '<C-s>', "<cmd>w<cr><esc>", { desc = 'Save File' })
+vim.keymap.set('s', '<C-s>', "<cmd>w<cr><esc>", { desc = 'Save File' })
 -- tab
 vim.keymap.set('n', '<leader><tab>l', "<cmd>tablast<cr>", { desc = 'Last Tab' })
 vim.keymap.set('n', '<leader><tab>f', "<cmd>tabfirst<cr>", { desc = 'First Tab' })
