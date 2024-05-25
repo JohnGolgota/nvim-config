870af0043af4040e9371b6c40c3f8dab3b83de9e
John Steban Lopez <104475729+JohnGolgota@users.noreply.github.com>
2024-05-25 16:21:22 +0200

    chore: me vale

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,25 +3,31 @@ return {
   "evanleck/vim-svelte",
   'nvim-lua/plenary.nvim',
   'nvim-pack/nvim-spectre',
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false
-      })
-      vim.keymap.set('i', '<tab>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false
-      })
-
-      vim.g.copilot_no_tab_map = false
-    end,
-  },
+  -- {
+  --   "github/copilot.vim",
+  --   config = function()
+  --     vim.keymap.set('i', '<Tab>', 'copilot#Accept("\\<CR>")', {
+  --       expr = true,
+  --       replace_keycodes = false
+  --     })
+  --     vim.keymap.set('i', '<tab>', 'copilot#Accept("\\<CR>")', {
+  --       expr = true,
+  --       replace_keycodes = false
+  --     })
+  --
+  --     vim.g.copilot_no_tab_map = false
+  --   end,
+  -- },
   {
     "supermaven-inc/supermaven-nvim",
     config = function()
-      require("supermaven-nvim").setup({})
+      require("supermaven-nvim").setup({
+        ketmaps = {
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
+        },
+      })
     end,
   },
 }
